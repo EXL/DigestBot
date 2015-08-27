3857755d04809638d7485a320a5e1a42461723bc
EXL <exlmotodev@gmail.com>
2015-08-27 16:24:23 +0600

    Initial commit

---
+++ DigestBot.js
@@ -0,0 +1,42 @@
+var TelegramBot = require('node-telegram-bot-api');
+var token = '';
+
+var options = {
+    polling: true
+};
+
+var bot = new TelegramBot(token, options);
+
+var countOfMessageWithDigest = 0;
+
+bot.getMe().then(function (me) {
+    console.log('Hello! My name is %s!', me.first_name);
+    console.log('My id is %s.', me.id)
+    console.log('And my username is @%s.', me.username);
+});
+
+bot.on('text', function(msg) {
+    var chatId = msg.chat.id
+
+    console.log(msg);
+    console.log(msg.text);
+    console.log(msg.text.indexOf('#digest'))
+
+    if (msg.text.indexOf('#digest') >= 0) {
+        console.log('Get message with #digest tag, save to stack');
+        countOfMessageWithDigest++;
+    }
+
+    if (msg.text === '/digest' || msg.text === '/Digest') {
+        console.log('Digest command received, sending digest stack...');
+        bot.sendMessage(chatId,
+                        getCountOfMessageWithDigest(),
+                        { caption: "I'm a bot!" });
+    }
+
+    console.log(countOfMessageWithDigest);
+});
+
+function getCountOfMessageWithDigest() {
+    return 'Count of digest messages is ' + countOfMessageWithDigest;
+}
