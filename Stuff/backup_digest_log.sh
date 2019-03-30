1661d3a239be8075e3cdebcf59aaa18c2f2fa045
EXL <exlmotodev@gmail.com>
2019-03-30 07:43:06 +0700

    Migrate to new host

--- Stuff/backup_digest_log.sh
+++ Stuff/backup_digest_log.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DAYMONTH=`date +"%Y%m%d_%H%M%S"`
-/bin/tar -czf /root/digestbot/BackUpDigestLog/backup_digest_log_$DAYMONTH.tar.gz -C /root/digestbot/DigestBot/ DigestBotStackLog.json
+/bin/tar -czf /srv/digestbot/BackUpDigestLog/backup_digest_log_$DAYMONTH.tar.gz -C /srv/digestbot/DigestBot/ DigestBotStackLog.json
 
 # Backup rotation, default 100 files
-FILES=100
-ls -1 /root/digestbot/BackUpDigestLog/backup_* | sort -r | tail -n +$(($FILES+1)) | xargs rm > /dev/null 2>&1
+# FILES=100
+# ls -1 /srv/digestbot/BackUpDigestLog/backup_* | sort -r | tail -n +$(($FILES+1)) | xargs rm > /dev/null 2>&1
